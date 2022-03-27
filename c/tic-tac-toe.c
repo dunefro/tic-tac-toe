460ca39d5cd9930a1deb2b318fab16c893099f60
Vedant Pareek <dunefro@gmail.com>
2022-03-27 13:52:41 +0530

    Directory structure changed

---
+++ c/tic-tac-toe.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int toss(void) {
+    time_t t;
+    srand((unsigned) time(&t));
+    return (rand() % 2 + 1);
+} 
+
+void displayGrid(char *arr) {
+    printf("\n");
+    for (int i=1;i<=9;i++) {
+        if (arr[i] == 'X' || arr[i] == 'O')
+            printf(" %c |",arr[i]);
+        else
+            printf(" %d |", i);
+        if(i%3 == 0) {
+            printf("\n");
+            printf("___|___|___|\n\n");
+        }
+    }
+}
+
+int verifyMove(char *arr, int grid) {
+    printf("Your move is %d\n", grid);
+    if (grid <=9 && grid >=1) {
+        if (arr[grid] != 'X' && arr[grid] != 'O' )
+            return 0;
+    }
+    return 1; // something wrong
+}
+
+int checkVictory() {
+    return 1;
+}
+
+int main() {
+    printf("Player 1 (X) - Player 2 (O)\n");
+    printf("Doing a toss: ");
+    int currentPlayerNumber = toss();
+    char currentPlayerSymbol;
+    printf("Player %d won\n", currentPlayerNumber);
+    int x = 0;
+    char arr[10];
+    int grid =0 ;
+    while(1) {
+        displayGrid(arr);
+        if (currentPlayerNumber == 1){
+            currentPlayerNumber = 2;
+            currentPlayerSymbol = 'O';
+        }
+        else {
+            currentPlayerNumber = 1;
+            currentPlayerSymbol = 'X';
+        }
+        printf("Player %d (%c), Enter your move: ", currentPlayerNumber, currentPlayerSymbol);
+        scanf("%d", &grid);
+        int check = verifyMove(arr, grid);
+        if (check) {
+            printf("Invalid Move\n");
+            return 1;
+        }
+        arr[grid] = currentPlayerSymbol;
+        if (checkVictory) {
+            break;
+        }
+    }
+    return 0;
+}
